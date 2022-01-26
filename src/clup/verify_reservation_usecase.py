54781ee3f66a97f32b725718dc1cf04434f9a811
dcmonti <d.monti11@campus.unimib.it>
2022-01-26 09:05:20 +0100

    implementata funzione verify_reservation e relativi tests

--- src/clup/verify_reservation_usecase.py
+++ src/clup/verify_reservation_usecase.py
@@ -1,2 +1,2 @@
 def verify_reservation(reservation_id, queue):
-    return True
+    return reservation_id in queue
--- tests/test_verify_reservation.py
+++ tests/test_verify_reservation.py
@@ -12,3 +12,29 @@ class TestBookUsecase(unittest.TestCase):
         is_reservation_valid = verify_reservation(reservation_id, queue)
         self.assertTrue(is_reservation_valid)
 
+    def test_reservation_not_valid_for_different_queue(self):
+        market_id = 1
+        queue1 = ()
+        queue2 = ()
+        reservation, queue1 = book(market_id, queue1)
+        reservation1_id, _ = reservation
+        is_reservation_valid_for_different_queue = verify_reservation(reservation1_id, queue2)
+        self.assertFalse(is_reservation_valid_for_different_queue)
+
+    def test_reservation_with_multiple_queue(self):
+        market1_id = 1
+        market2_id = 2
+        queue1 = ()
+        queue2 = ()
+        reservation1, queue1 = book(market1_id, queue1)
+        reservation2, queue2 = book(market2_id, queue2)
+        reservation1_id, _ = reservation1
+        reservation2_id, _ = reservation2
+        is_reservation1_valid_for_queue1 = verify_reservation(reservation1_id, queue1)
+        is_reservation2_valid_for_queue2 = verify_reservation(reservation2_id, queue2)
+        is_reservation1_valid_for_queue2 = verify_reservation(reservation1_id, queue2)
+        is_reservation2_valid_for_queue1 = verify_reservation(reservation2_id, queue1)
+        self.assertTrue(is_reservation1_valid_for_queue1)
+        self.assertTrue(is_reservation2_valid_for_queue2)
+        self.assertFalse(is_reservation1_valid_for_queue2)
+        self.assertFalse(is_reservation2_valid_for_queue1)
