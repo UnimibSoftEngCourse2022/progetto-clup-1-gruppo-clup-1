31f097838f8280c6bbdf868f2d953587533e86a9
dcmonti <d.monti11@campus.unimib.it>
2022-01-27 10:59:45 +0100

    aggiornati requisiti e risolto problema sicurezza

--- src/clup/app/__init__.py
+++ src/clup/app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fd27835ea6ab3d57137c0e3e'
-app.config['WTF_CSRF_ENABLED'] = True
+csrf = CSRFProtect()
+csrf.init_app(app) # Compliant
+
 
 from app import views
