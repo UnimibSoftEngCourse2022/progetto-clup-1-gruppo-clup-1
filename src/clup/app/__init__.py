ff2a48bee8f854f5c0c430d6a547b8683e87810c
dcmonti <d.monti11@campus.unimib.it>
2022-01-27 10:45:38 +0100

    risolto problema di sicurezza

--- src/clup/app/__init__.py
+++ src/clup/app/__init__.py
@@ -2,5 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fd27835ea6ab3d57137c0e3e'
+app.config['WTF_CSRF_ENABLED'] = True
 
-from src.clup.app import views
+from app import views
