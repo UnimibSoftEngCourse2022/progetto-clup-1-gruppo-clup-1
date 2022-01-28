ceec93bed235b5b984431baf4d0ca2e8aeaaa3f2
RFrig16 <93283860+RFrig16@users.noreply.github.com>
2022-01-28 16:05:28 +0100

    store entities added and refactoring

--- src/clup/usecases/add_store_usecase.py
+++ src/clup/usecases/add_store_usecase.py
@@ -1,10 +1,26 @@
+import uuid
+from src.clup.entities.store import Store
 
 class AddStoreUseCase:
     def __init__(self, store_provider):
         self.store_provider = store_provider
 
-    def execute(self, store_id):
-        if store_id in self.store_provider.get_stores():
-            raise ValueError('store_id already present')
+    def execute(self, name, address, capacity):
+        store_id = str(uuid.uuid1())
+        store = Store(store_id, name, address, capacity)
+        stores = self.store_provider.get_stores()
+        for store_item in stores:
+            if store_item.name == store.name and store_item.address == store.address:
+                raise ValueError('store alredy present')
 
-        self.store_provider.add_store(store_id)
+        if not name:
+            raise ValueError('store name is empty')
+
+        if not address:
+            raise ValueError('store address is empty')
+
+        if capacity < 0:
+            raise ValueError('capacity is negative')
+
+        self.store_provider.add_store(store)
+        return store
---
+++ tests/test_add_store_usecase.py
@@ -0,0 +1,70 @@
+import unittest
+
+from src.clup.usecases.add_store_usecase import AddStoreUseCase
+
+
+class MockStoreProvider:
+    def __init__(self):
+        self.stores = []
+
+    def get_stores(self):
+        return self.stores
+
+    def add_store(self, store):
+        self.stores.append(store)
+
+
+class TestAddStoreUseCase(unittest.TestCase):
+    def test_store_is_added_to_stores(self):
+        store_provider = MockStoreProvider()
+        u = AddStoreUseCase(store_provider)
+        name = "store"
+        address = "address"
+        capacity = 1
+
+        store = u.execute(name, address, capacity)
+        stores = store_provider.get_stores()
+        
+        self.assertTrue(store in stores)
+
+    def test_adding_same_store_twice_throws_value_error(self):
+        store_provider = MockStoreProvider()
+        u = AddStoreUseCase(store_provider)
+        name = "store"
+        address = "address"
+        capacity = 1
+
+        u.execute(name, address, capacity)
+        
+        with self.assertRaises(ValueError):
+            u.execute(name, address, 5)
+
+    def test_name_field_of_a_store_is_empty(self):
+        store_provider = MockStoreProvider()
+        u = AddStoreUseCase(store_provider)
+        name = ""
+        address = "address"
+        capacity = 1
+
+        with self.assertRaises(ValueError):
+            u.execute(name, address, capacity)
+
+    def test_address_field_of_a_store_is_empty(self):
+        store_provider = MockStoreProvider()
+        u = AddStoreUseCase(store_provider)
+        name = "store"
+        address = ""
+        capacity = 1
+
+        with self.assertRaises(ValueError):
+            u.execute(name, address, capacity)
+
+    def test_capacity_not_negative(self):
+        store_provider = MockStoreProvider()
+        u = AddStoreUseCase(store_provider)
+        name = "store"
+        address = "address"
+        capacity = -1
+
+        with self.assertRaises(ValueError):
+            u.execute(name, address, capacity)
