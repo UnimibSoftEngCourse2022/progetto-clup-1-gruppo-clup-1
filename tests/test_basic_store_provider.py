5d78aff1fd39ba455b0313efd8e81fef7d97eab9
mattia <m.napoli16@campus.unimib.it>
2022-01-28 16:25:43 +0100

    refactored basicstoreprovider to use store obj and new add_to_queue. todo file added to gitignore

--- tests/test_basic_store_provider.py
+++ tests/test_basic_store_provider.py
@@ -1,81 +1,76 @@
 import unittest
+
+from src.clup.entities.store import Store
 from src.clup.providers.basic_store_provider import BasicStoreProvider
 
-class TestBasicStoreProvider(unittest.TestCase):
-    
-    def test_is_empty_after_initialization(self):
-        bsp = BasicStoreProvider()
 
-        stores = bsp.get_stores()
+class TestBasicStoreProvider(unittest.TestCase):
+    def setUp(self):
+        self.bsp = BasicStoreProvider()
+        self.store1 = Store(1, None, None, None)
+        self.store2 = Store(2, None, None, None)
+
+    def test_is_empty_after_initialization(self):
+        stores = self.bsp.get_stores()
         is_empty = len(stores) == 0
 
         self.assertTrue(is_empty)
     
-    def test_correct_element_added(self):
-        bsp = BasicStoreProvider()
-        store_id = 1
+    def test_element_is_added(self):
+        self.bsp.add_store(self.store1)
+        stores = self.bsp.get_stores()
 
-        bsp.add_store(store_id)
-        stores = bsp.get_stores()
-        is_id_in_stores = store_id in stores
-
-        self.assertTrue(is_id_in_stores)
+        self.assertTrue(self.store1 in stores)
 
     def test_only_corrects_elements_added(self):
-        bsp = BasicStoreProvider()
-        store1_id = 1
-        store2_id = 2
+        self.bsp.add_store(self.store1)
+        self.bsp.add_store(self.store2)
+        stores = self.bsp.get_stores()
 
-        bsp.add_store(store1_id)
-        bsp.add_store(store2_id)
-        stores = bsp.get_stores()
-        is_correct_length = len(stores) == 2
+        self.assertTrue(len(stores) == 2)
 
-        self.assertTrue(is_correct_length)
+    def test_add_store_twice_throws(self):
+        self.bsp.add_store(self.store1)
 
-    def test_add_store_twice_froze(self):
-        bsp = BasicStoreProvider()
-        store_id = 1
-
-        bsp.add_store(store_id)
         with self.assertRaises(ValueError):
-            bsp.add_store(store_id)
+            self.bsp.add_store(self.store1)
     
+
 class TestQueuesOfBasicStoreProvider(unittest.TestCase):
+    def setUp(self):
+        self.bsp = BasicStoreProvider()
+        self.store1 = Store(1, None, None, None)
+        self.store2 = Store(2, None, None, None)
 
     def test_queue_of_new_store_is_empty(self):
-        bsp = BasicStoreProvider()
         store_id = 1
-        bsp.add_store(store_id)
+        self.bsp.add_store(self.store1)
 
-        store_queue = bsp.get_queue(store_id)
+        store_queue = self.bsp.get_queue(self.store1.id)
         is_empty = len(store_queue) == 0
 
         self.assertTrue(is_empty)
 
-    def test_set_a_new_queue(self):
-        bsp = BasicStoreProvider()
-        store_id = 1
-        bsp.add_store(store_id)
+    def test_add_element_to_queue(self):
+        self.bsp.add_store(self.store1)
 
-        new_queue = [1]
-        bsp.set_queue(store_id, new_queue)
-        queue = bsp.get_queue(store_id)
+        reservation = (1, 2, 3)
+        self.bsp.add_to_queue(self.store1.id, reservation)
+        queue = self.bsp.get_queue(self.store1.id)
 
-        self.assertEqual(new_queue, queue)
+        self.assertTrue(reservation in queue)
 
     def test_different_queues_for_different_stores(self):
-        bsp = BasicStoreProvider()
-        store1_id = 1
-        store2_id = 3
-        bsp.add_store(store1_id)
-        bsp.add_store(store2_id)
-        new_queue1 = [1]
-        new_queue2 = [2]
+        self.bsp.add_store(self.store1)
+        self.bsp.add_store(self.store2)
+        reservation1 = (1, 2, 3)
+        reservation2 = (4, 5, 6)
         
-        bsp.set_queue(store1_id, new_queue1)
-        bsp.set_queue(store2_id, new_queue2)
-        queue1 = bsp.get_queue(store1_id)
-        queue2 = bsp.get_queue(store2_id)
+        self.bsp.add_to_queue(self.store1.id, reservation1)
+        self.bsp.add_to_queue(self.store2.id, reservation2)
+        queue1 = self.bsp.get_queue(self.store1.id)
+        queue2 = self.bsp.get_queue(self.store2.id)
 
+        self.assertTrue(reservation1 in queue1)
+        self.assertTrue(reservation2 in queue2)
         self.assertNotEqual(queue1, queue2)
