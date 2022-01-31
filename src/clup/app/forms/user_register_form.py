0cfd791c94f6a413b795f0f730ef3554ae56c5a6
DavideSoldati <d.soldati2@campus.unimib.it>
2022-01-31 11:00:49 +0100

    controllo user_id e password non vuoti

--- src/clup/app/forms/user_register_form.py
+++ src/clup/app/forms/user_register_form.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import EqualTo
+from wtforms.validators import EqualTo, DataRequired
 
 
 class UserRegisterForm(FlaskForm):
-    user_id = StringField(label='User Name')
-    password1 = StringField(label='Password')
-    password2 = StringField(label='Confirm Password', validators=[EqualTo('password1')])
+    user_id = StringField(label='User Name', validators=[DataRequired()])
+    password1 = StringField(label='Password', validators=[DataRequired()])
+    password2 = StringField(label='Confirm Password', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Register')
