0d8a8f02ca1087a65788a037ea11725786a99405
mattia <m.napoli16@campus.unimib.it>
2022-02-02 12:20:48 +0100

    enableusecase is now for arbitray reservations

--- src/clup/usecases/enable_reservation_usecase.py
+++ src/clup/usecases/enable_reservation_usecase.py
@@ -2,9 +2,9 @@ class EnableReservationUseCase:
     def __init__(self, store_provider):
         self.store_provider = store_provider
 
-    def execute(self, store_id):
+    def execute(self, store_id, reservation):
         waiting_queue = self.store_provider.get_waiting_queue(store_id)
         active_pool = self.store_provider.get_active_pool(store_id)
 
-        reservation = waiting_queue.pop()
+        waiting_queue.remove(reservation)
         active_pool.add(reservation)
--- tests/usecases/test_enable_reservation_usecase.py
+++ tests/usecases/test_enable_reservation_usecase.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.clup.entities.exceptions import MaxCapacityReachedError
+from src.clup.entities.exceptions import MaxCapacityReachedError, EmptyQueueError
 from src.clup.entities.waiting_queue import WaitingQueue
 from src.clup.entities.active_pool import ActivePool
 from src.clup.providers.queue_provider_abc import QueueProvider
@@ -25,26 +25,43 @@ class TestEnableReservationUseCase(unittest.TestCase):
         self.queue_provider = MockQueueProvider()
         self.u = EnableReservationUseCase(self.queue_provider)
 
-    def test_enable_pops_from_queue_and_add_to_pool(self):
+    def test_enable_takes_element_from_queue_and_add_to_pool(self):
         store_id = 1
         self.queue_provider.get_waiting_queue(store_id).push('a')
 
-        self.u.execute(store_id)
+        self.u.execute(store_id, 'a')
 
         self.assertTrue('a' in self.queue_provider.get_active_pool(store_id))
+
+    def test_takes_element_in_arbitrary_position_and_add_to_pool(self):
+        store_id = 1
+        self.queue_provider.get_waiting_queue(store_id).push('a')
+        self.queue_provider.get_waiting_queue(store_id).push('b')
+        self.queue_provider.get_waiting_queue(store_id).push('c')
+
+        self.u.execute(store_id, 'b')
+
+        self.assertTrue('b' in self.queue_provider.get_active_pool(store_id))
 
     def test_enable_on_empty_queue_throws(self):
         store_id = 1
 
-        with self.assertRaises(IndexError):
-            self.u.execute(store_id)
+        with self.assertRaises(EmptyQueueError):
+            self.u.execute(store_id, 'a')
+
+    def test_throws_if_element_is_not_in_waiting_queue(self):
+        store_id = 1
+        self.queue_provider.get_waiting_queue(store_id).push('a')
+
+        with self.assertRaises(ValueError):
+            self.u.execute(store_id, 'z')
 
     def test_enable_on_full_active_pool_throws(self):
         store_id = 1
         self.queue_provider.get_active_pool(store_id).capacity = 1
         self.queue_provider.get_waiting_queue(store_id).push('a')
         self.queue_provider.get_waiting_queue(store_id).push('b')
-        self.u.execute(store_id)
+        self.u.execute(store_id, 'b')
 
         with self.assertRaises(MaxCapacityReachedError):
-            self.u.execute(store_id)
+            self.u.execute(store_id, 'a')
