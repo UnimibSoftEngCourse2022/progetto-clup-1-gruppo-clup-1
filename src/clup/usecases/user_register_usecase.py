c230ab35af290a41c2a9f2abd1acff37a71dab79
dcmonti <d.monti11@campus.unimib.it>
2022-02-02 10:58:59 +0100

    modifica user

--- src/clup/usecases/user_register_usecase.py
+++ src/clup/usecases/user_register_usecase.py
@@ -1,16 +1,16 @@
 from src.clup.entities.user import User
+import uuid
 
 
 class UserRegisterUsecase:
     def __init__(self, user_provider):
         self.user_provider = user_provider
 
-    def execute(self, user):
-        if type(user) is not User:
-            raise ValueError("you must be an user")
-        if user.id in [usr.id for usr in self.user_provider.get_users()]:
-            raise ValueError('user_id already present')
-        if not user.id or not user.password:
+    def execute(self, username, password):
+        user_id = uuid.uuid1()
+        if username in [usr.username for usr in self.user_provider.get_users()]:
+            raise ValueError('username already present')
+        if not username or not password:
             raise ValueError('Null type not valid')
-
+        user = User(user_id, username, password)
         self.user_provider.add_user(user)
