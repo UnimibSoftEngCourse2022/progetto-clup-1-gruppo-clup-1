d3bec3cb1c4d56ec43ee2984a42c6f9a79d5b965
dcmonti <d.monti11@campus.unimib.it>
2022-02-07 11:01:44 +0100

    implementazione employee provider

---
+++ src/clup/providers/employee_provider.py
@@ -0,0 +1,37 @@
+class EmployeeProvider:
+    def __init__(self):
+        self.store_managers = {}
+        self.store_admins = {}
+
+    def add_store_manager(self, store_id, manager_id):
+        if store_id in self.store_managers.keys():
+            raise ValueError("store manager already present for this store")
+        self.store_managers[store_id] = manager_id
+
+    def add_store_admin(self, store_id, admin_id):
+        if store_id in self.store_admins.keys():
+            self.store_admins[store_id].append(admin_id)
+        else:
+            self.store_admins[store_id] = [admin_id]
+
+    def get_store_manager_id(self, store_id):
+        if store_id not in self.store_managers.keys():
+            raise ValueError("store not existing")
+        return self.store_managers[store_id]
+
+    def get_store_admins_id(self, store_id):
+        if store_id not in self.store_managers.keys():
+            raise ValueError("store not existing")
+        return self.store_admins[store_id]
+    
+    def get_store_id_from_manager_id(self, manager_id):
+        for store_id, mg_id in self.store_managers:
+            if mg_id == manager_id:
+                return store_id
+        raise ValueError("manager_id not present")
+
+    def get_store_id_from_admin_id(self, admin_id):
+        for store_id, admin_list in self.store_admins:
+            if admin_id in admin_list:
+                return store_id
+        raise ValueError("admin_id not present")
