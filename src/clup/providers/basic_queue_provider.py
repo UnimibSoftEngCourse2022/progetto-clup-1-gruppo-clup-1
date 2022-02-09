9589b4da74bee4b1298b198a9ddf8ff32f162b6a
mattia <m.napoli16@campus.unimib.it>
2022-02-09 12:11:58 +0100

    basicqueueprovider now fullfil abc interface

--- src/clup/providers/basic_queue_provider.py
+++ src/clup/providers/basic_queue_provider.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 
 from src.clup.entities.active_pool import ActivePool
+from src.clup.entities.store_active_pool import StoreActivePool
 from src.clup.entities.waiting_queue import WaitingQueue
 from src.clup.providers.queue_provider_abc import QueueProvider
 
@@ -9,9 +10,13 @@ class BasicQueueProvider(QueueProvider):
     def __init__(self):
         self.queues = defaultdict(WaitingQueue)
         self.pools = defaultdict(ActivePool)
+        self.store_pools = defaultdict(StoreActivePool)
 
-    def get_waiting_queue(self, store_id):
-        return self.queues[store_id]
+    def get_waiting_queue(self, aisle_id):
+        return self.queues[aisle_id]
 
-    def get_aisle_pool(self, store_id):
-        return self.pools[store_id]
+    def get_aisle_pool(self, aisle_id):
+        return self.pools[aisle_id]
+
+    def get_store_pool(self, store_id):
+        return self.store_pools[store_id]
--- tests/providers/test_basic_queue_provider.py
+++ tests/providers/test_basic_queue_provider.py
@@ -7,37 +7,46 @@ class TestBasicQueueProvider(unittest.TestCase):
     def setUp(self):
         self.bqp = BasicQueueProvider()
 
-    @unittest.skip('incomplete')
-    def test_unexistent_store_id_gets_empty_queue_and_pool(self):
+    def test_by_default_gets_empty_queue_and_pools(self):
         store_id = 1
-        queue = self.bqp.get_waiting_queue(store_id)
-        pool = self.bqp.get_aisle_pool(store_id)
+        aisle_id = 10
+        queue = self.bqp.get_waiting_queue(aisle_id)
+        pool = self.bqp.get_aisle_pool(aisle_id)
+        store_pool = self.bqp.get_store_pool(store_id)
 
         self.assertEqual(len(queue), 0)
         self.assertEqual(len(pool), 0)
+        self.assertEqual(len(store_pool.pool), 0)
+        self.assertEqual(len(store_pool.to_free), 0)
 
-    @unittest.skip('incomplete')
-    def test_modifications_on_store_queues_are_presisted(self):
-        store_id = 1
-        queue = self.bqp.get_waiting_queue(store_id)
-        pool = self.bqp.get_aisle_pool(store_id)
+    def test_modifications_on_store_queues_are_persistent(self):
+        aisle_id = 1
+        queue = self.bqp.get_waiting_queue(aisle_id)
+        pool = self.bqp.get_aisle_pool(aisle_id)
+        store_id = 10
+        store_pool = self.bqp.get_store_pool(store_id)
 
         queue.push('a')
         pool.capacity = 10
         pool.add('b')
+        store_pool.add('c')
 
-        self.assertTrue('a' in self.bqp.get_waiting_queue(store_id))
-        self.assertTrue('b' in self.bqp.get_aisle_pool(store_id))
-        self.assertEqual(self.bqp.get_aisle_pool(store_id).capacity, 10)
+        self.assertTrue('a' in self.bqp.get_waiting_queue(aisle_id))
+        self.assertTrue('b' in self.bqp.get_aisle_pool(aisle_id))
+        self.assertEqual(self.bqp.get_aisle_pool(aisle_id).capacity, 10)
+        self.assertTrue('c' in self.bqp.get_store_pool(store_id).pool)
 
-    @unittest.skip('incomplete')
     def test_modifications_on_different_stores_act_on_different_queues(self):
-        store1_id = 1
-        store2_id = 2
-        queue1 = self.bqp.get_waiting_queue(store1_id)
-        pool1 = self.bqp.get_aisle_pool(store1_id)
-        queue2 = self.bqp.get_waiting_queue(store2_id)
-        pool2 = self.bqp.get_aisle_pool(store2_id)
+        aisle1_id = 1
+        aisle2_id = 2
+        queue1 = self.bqp.get_waiting_queue(aisle1_id)
+        pool1 = self.bqp.get_aisle_pool(aisle1_id)
+        queue2 = self.bqp.get_waiting_queue(aisle2_id)
+        pool2 = self.bqp.get_aisle_pool(aisle2_id)
+        store1_id = 10
+        store2_id = 20
+        store1_pool = self.bqp.get_store_pool(store1_id)
+        store2_pool = self.bqp.get_store_pool(store2_id)
 
         pool1.capacity = 5
         pool2.capacity = 9
@@ -45,9 +54,13 @@ class TestBasicQueueProvider(unittest.TestCase):
         queue2.push('b')
         pool1.add('c')
         pool2.add('d')
+        store1_pool.add('e')
+        store2_pool.add('f')
 
-        self.assertTrue('a' not in self.bqp.get_waiting_queue(store2_id))
-        self.assertTrue('b' not in self.bqp.get_waiting_queue(store1_id))
-        self.assertTrue('c' not in self.bqp.get_aisle_pool(store2_id))
-        self.assertTrue('d' not in self.bqp.get_aisle_pool(store1_id))
+        self.assertTrue('a' not in self.bqp.get_waiting_queue(aisle2_id))
+        self.assertTrue('b' not in self.bqp.get_waiting_queue(aisle1_id))
+        self.assertTrue('c' not in self.bqp.get_aisle_pool(aisle2_id))
+        self.assertTrue('d' not in self.bqp.get_aisle_pool(aisle1_id))
         self.assertNotEqual(pool1.capacity, pool2.capacity)
+        self.assertTrue('e' not in self.bqp.get_store_pool(store2_id).pool)
+        self.assertTrue('f' not in self.bqp.get_store_pool(store1_id).pool)
