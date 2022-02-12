56c35ebdd364bd07ed17dcca03caa0078bd4a64f
dcmonti <d.monti11@campus.unimib.it>
2022-02-12 13:04:43 +0100

    risolto problema test

--- src/clup/providers/basic_reservation_provider.py
+++ src/clup/providers/basic_reservation_provider.py
@@ -6,9 +6,8 @@ class BasicReservationProvider:
         return self.reservations
 
     def add_reservation(self, reservation):
-        # if reservation.id in (r.id for r in self.reservations):
-        # raise ValueError('reservation id already present')
-
+        if reservation.id in (r.id for r in self.reservations):
+            raise ValueError('reservation id already present')
         self.reservations.append(reservation)
 
     # TODO: get_reservations_with_id(reservation_id)
