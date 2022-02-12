29c78564aa404c3166a1387899a87a97ee0fd60f
mattia <m.napoli16@campus.unimib.it>
2022-02-12 13:51:21 +0100

    sqlitereservationprovider tested and refactored

--- src/clup/providers/sqlite_reservation_provider.py
+++ src/clup/providers/sqlite_reservation_provider.py
@@ -1,63 +1,56 @@
 from sqlalchemy.orm import Session
 
-from src.clup.database import Reservation, engine
-from src.clup.entities import reservation
+import src.clup.database.models as models
+from src.clup.entities.reservation import Reservation
 
 
 class SqliteReservationProvider:
-    def get_reservations(self):
-        db_session = Session(engine)
-        reservations = db_session.query(Reservation).all()
-        reservations_ent = []
-        for res_db in reservations:
-            res_ent = reservation.Reservation(id=res_db.uuid, user_id=res_db.user_id, aisle_id=res_db.aisle_id)
-            reservations_ent.append(res_ent)
-        return reservations_ent
+    def __init__(self, engine):
+        self.engine = engine
 
-    def get_user_reservation(self, user_id):
-        db_session = Session(engine)
-        query = db_session.query(Reservation).filter(Reservation.user_id == user_id)
-        reservations = query.all()
-        user_res = []
-        for r in reservations:
-            res = reservation.Reservation(id=r.uuid, user_id=r.user_id, aisle_id=r.aisle_id)
-            user_res.append(res)
-        return user_res
+    def get_reservations(self):
+        with Session(self.engine) as session, session.begin():
+            model_reservations = session.query(models.Reservation).all()
+            reservations = [Reservation(mr.uuid, mr.aisle_id, mr.user_id)
+                            for mr in model_reservations]
+            return reservations
+
+    def get_user_reservations(self, user_id):
+        with Session(self.engine) as session, session.begin():
+            query = session.query(models.Reservation).\
+                filter(models.Reservation.user_id == user_id)
+            model_reservations = query.all()
+            reservations = [Reservation(mr.uuid, mr.aisle_id, mr.user_id)
+                            for mr in model_reservations]
+            return reservations
 
     def get_reservations_with_id(self, reservation_id):
-        db_session = Session(engine)
-        reservations_same_id = db_session.query(Reservation).filter(Reservation.uuid == reservation_id).all()
-        res_ents = []
-        for res_w_id in reservations_same_id:
-            res_ent = reservation.Reservation(id=res_w_id.uuid, aisle_id=res_w_id.aisle_id, user_id=res_w_id.user_id)
-            res_ents.append(res_ent)
-        return res_ents
+        with Session(self.engine) as session, session.begin():
+            query = session.query(models.Reservation).\
+                filter(models.Reservation.uuid == reservation_id)
+            model_reservations = query.all()
+            reservations = [Reservation(mr.uuid, mr.aisle_id, mr.user_id)
+                            for mr in model_reservations]
+            return reservations
 
-    def add_reservation(self, reservation_ent):
-        db_session = Session(engine)
-        new_reservation = Reservation(uuid=reservation_ent.id, aisle_id=reservation_ent.aisle_id,
-                                      user_id=reservation_ent.user_id)
-        db_session.add(new_reservation)
-        db_session.commit()
+    def add_reservation(self, reservation):
+        with Session(self.engine) as session, session.begin():
+            model_reservation = models.Reservation(
+                uuid=reservation.id,
+                aisle_id=reservation.aisle_id,
+                user_id=reservation.user_id,
+            )
+            session.add(model_reservation)
 
     def remove_reservation(self, reservation_id):
-        db_session = Session(engine)
-        db_session.query(Reservation).filter(Reservation.uuid == reservation_id).delete()
-        db_session.commit()
+        with Session(self.engine) as session, session.begin():
+            query = session.query(models.Reservation).\
+                filter(models.Reservation.uuid == reservation_id)
+            query.delete()
 
     def remove_reservation_from_aisle(self, reservation_id, aisle_id):
-        db_session = Session(engine)
-        db_session.query(Reservation).filter((Reservation.uuid == reservation_id), (Reservation.aisle_id == aisle_id)) \
-            .delete()
-        db_session.commit()
-
-
-rp = SqliteReservationProvider()
-print(rp.get_reservations())
-print(rp.get_user_reservation(1))
-print(rp.get_reservations_with_id(2000))
-rp.add_reservation(reservation.Reservation(id=2000, aisle_id=30, user_id=2))
-print(rp.get_reservations_with_id(2000))
-print(rp.get_reservations())
-rp.remove_reservation_from_aisle(2000, 30)
-print(rp.get_reservations())
+        with Session(self.engine) as session, session.begin():
+            query = session.query(models.Reservation).\
+                filter(models.Reservation.uuid == reservation_id).\
+                filter(models.Reservation.aisle_id == aisle_id)
+            query.delete()
