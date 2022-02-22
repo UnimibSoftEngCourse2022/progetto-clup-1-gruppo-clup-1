1d8506e9794e95cb8b5a36ad8af29ae00c4820bd
DavideSoldati <d.soldati2@campus.unimib.it>
2022-02-22 13:11:43 +0100

    Store manager view

--- src/clup/flaskr/store_manager.py
+++ src/clup/flaskr/store_manager.py
@@ -1,12 +1,15 @@
 from flask import Blueprint, flash, redirect, url_for, render_template
 from flask_login import login_required, current_user
 
-from src.clup.flaskr import global_setup
+from src.clup.flaskr import global_setup as setup
+from src.clup.usecases.add_store_usecase import AddStoreUseCase
 from src.clup.usecases.create_store_manager import CreateStoreManagerUseCase
+from src.clup.usecases.load_store_info_usecase import LoadStoreInfoUseCase
 from src.clup.usecases.load_store_manager_usecase import LoadStoreManagerUseCase
 from src.clup.usecases.store_manager_register_usecase import StoreManagerRegisterUseCase
 
 bp = Blueprint('store_manager', __name__)
+liu = LoadStoreInfoUseCase(setup.store_provider, setup.aisle_provider)
 
 
 def check_correct_account_type(requested_type):
@@ -18,10 +21,13 @@ def check_correct_account_type(requested_type):
 
 @bp.route('/manager/data/init')
 def data_init():
-    csm = CreateStoreManagerUseCase(global_setup.store_manager_provider)
-    csm.execute('secret_key')
-    rsm = StoreManagerRegisterUseCase(global_setup.store_manager_provider)
+    csm = CreateStoreManagerUseCase(setup.store_manager_provider)
+    id = csm.execute('secret_key')
+    rsm = StoreManagerRegisterUseCase(setup.store_manager_provider)
     rsm.execute('secret_key', 'boss', 'prova')
+    asu = AddStoreUseCase(setup.store_provider)
+    asu.execute('Tigros', 'Solbiate Arno', id)
+    asu.execute('Carrefour', 'Jerago', id)
     return redirect(url_for('store_manager.home'))
 
 
@@ -31,15 +37,18 @@ def home():
     if not check_correct_account_type('store_manager'):
         flash(f"unauthorized to visit this page, login as a store manager", category='danger')
         return redirect(url_for('auth.login'))
-    lsm = LoadStoreManagerUseCase(global_setup.store_manager_provider)
+    lsm = LoadStoreManagerUseCase(setup.store_manager_provider)
     store_manager = lsm.execute(current_user.id)
-    return render_template('store_manager_home.html', sm=store_manager)
+    stores = setup.store_provider.get_stores_from_manager_id(store_manager.id)
+    return render_template('store_manager_home.html', sm=store_manager, stores=stores)
 
 
 @bp.route('/storemanager/stores/<store_id>')
 @login_required
 def store_info(store_id):
-    return '<h1>Store Manager Store Info Page</h1>'
+    store_info = liu.execute(store_id)
+    print(store_info)
+    return render_template('store_view_manager.html',  store_info=store_info)
 
 
 @bp.route('/storemanager/stores', methods=['POST'])
