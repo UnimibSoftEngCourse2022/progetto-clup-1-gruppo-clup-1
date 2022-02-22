aba633dd24f2b44758b2e21a01e7825b1ea02ab0
mattia <m.napoli16@campus.unimib.it>
2022-02-22 10:04:30 +0100

    laneprovider bug fixed

--- src/clup/flaskr/global_setup.py
+++ src/clup/flaskr/global_setup.py
@@ -10,6 +10,7 @@ from src.clup.providers.sqlite_reservation_provider \
 from src.clup.providers.sqlite_store_provider import SqliteStoreProvider
 from src.clup.providers.sqlite_user_provider import SqliteUserProvider
 from src.clup.providers.sqlite_admin_provider import SqliteAdminProvider
+from src.clup.usecases.init_lanes import InitLanes
 
 user_provider = SqliteUserProvider(engine)
 admin_provider = SqliteAdminProvider(engine)
@@ -18,5 +19,9 @@ store_manager_provider = SqliteStoreManagerProvider(engine)
 store_provider = SqliteStoreProvider(engine)
 aisle_provider = SqliteAisleProvider(engine)
 lane_provider = BasicLaneProvider()
+
 reservation_provider = SqliteReservationProvider(engine)
 appointment_provider = SqliteAppointmentProvider(engine)
+
+il = InitLanes(lane_provider, aisle_provider)
+il.execute()
