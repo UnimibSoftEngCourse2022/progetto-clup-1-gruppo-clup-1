38800b97a2ea3b41f4a03deca7ca6a930266544f
DavideSoldati <d.soldati2@campus.unimib.it>
2022-02-24 22:16:02 +0100

    resolve security problem

--- src/clup/flaskr/users.py
+++ src/clup/flaskr/users.py
@@ -2,7 +2,7 @@ from flask import (
     Blueprint,
     render_template,
     url_for,
-    redirect,
+    redirect, request,
 )
 from flask_login import login_required, current_user
 
@@ -25,7 +25,7 @@ def user_reservation(store_id):
     user_data = LoadUserUseCase(setup.user_provider).execute(u_id)
     form = UserReservationForm()
 
-    if form.validate_on_submit():
+    if form.validate_on_submit() and request.method == 'POST':
         reservation = mru.execute(store_id, u_id)
         return redirect(url_for('users.user_make_reservation', store_id=store_id, user=user_data, form=form,
                                 reservation_id=reservation.id))
@@ -33,7 +33,7 @@ def user_reservation(store_id):
     return render_template('user_reservation.html', store=store_id, user=user_data, form=form)
 
 
-@bp.route('/reservation/<store_id>/<reservation_id>', methods=['GET', 'POST'])
+@bp.route('/reservation/<store_id>/<reservation_id>')
 def user_make_reservation(store_id, reservation_id):
     u_id = current_user.get_id()
     user_data = LoadUserUseCase(setup.user_provider).execute(u_id)
